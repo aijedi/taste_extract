2b5f6e7fa705756bbc4d20beadde2b0d96d04260
P Tanush <tanush.pas25@gmail.com>
2018-07-03 13:03:06 +0530

    Add files via upload

---
+++ tagger.py
@@ -0,0 +1,28 @@
+import spacy
+import plac
+import random
+from pathlib import Path
+import sys
+
+TRAIN_DATA =[
+
+u"To be completely fair, the only redeeming factor was the food, which was above average, but couldn't make up for all the other deficiencies of Teodora.",
+
+u"Not only was the food outstanding, but the little 'perks' were great.",
+
+u"It is very overpriced and not very tasty."
+] 
+
+
+def tagger(path):
+	# output_dir = Path('/home/tanush/Desktop/Projects/NER Project/Taste Extractor/en_rev_taste')
+	output_dir = Path(path)
+	# test the saved model
+	print("Loading from", output_dir)
+	nlp2 = spacy.load(output_dir)
+	for text in TRAIN_DATA:
+		doc = nlp2(text)
+		print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
+
+if __name__ == '__main__':
+	tagger(sys.argv[1])
